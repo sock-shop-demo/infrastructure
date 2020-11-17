78640413864de8a56334b8922febd85fb3e0a43b
lautaro bringasq <lm.bringas98@gmail.com>
2020-11-17 11:24:16 -0300

    ❤️

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "s3" {
-    endpoint = "nyc3.digitaloceanspaces.com"
-    region = "us-west-1"
-    key = "terraform.tfstate"
-    skip_requesting_account_id = true
+    endpoint                    = "nyc3.digitaloceanspaces.com"
+    region                      = "us-west-1"
+    key                         = "terraform.tfstate"
+    skip_requesting_account_id  = true
     skip_credentials_validation = true
-    skip_get_ec2_platforms = true
-    skip_metadata_api_check = true
+    skip_get_ec2_platforms      = true
+    skip_metadata_api_check     = true
   }
   required_providers {
     github = {
